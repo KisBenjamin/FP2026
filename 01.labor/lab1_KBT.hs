32452febb2a739acd89949748873cf5c18d0e71e
Kis Beni <kis.beniamin.tibor@student.ms.sapientia.ro>
2026-02-26 09:46:12 +0000

    Lab2

--- 01.labor/lab1_KBT.hs
+++ 01.labor/lab1_KBT.hs
@@ -120,19 +120,89 @@ fakt3 res n
     | otherwise = fakt3 (res * n) (n-1)
 -- - az x szám n-ik hatványát, ha a kitevő pozitív szám (3 módszer).
 
+hatvanyX x n
+    | n < 0 = error "neg. kitevo"
+    | otherwise = x ** n
+
+hatvanyX2 x n
+    | n < 0 = error "neg. kitevo"
+    | otherwise = x ^ n
+
+hatvanyX3 x n
+    | n < 0 = error "neg. kitevo"
+    | n == 0 = 1
+    | otherwise = x * hatvanyX3 x (n-1)
+
 -- II. Könyvtárfüggvények használata nélkül, illetve halmazkifejezéseket alkalmazva, definiáljuk azt a függvényt, amely meghatározza:
 
 -- - az első n természetes szám negyzetgyökét,
+
+negyzetgyok n = [sqrt i | i <- [1,1.10 .. n]]
 -- - az első n négyzetszámot,
+
+negyzetszam n = [i ^ 2 | i <- [1 .. n]]
 -- - az első n természetes szám köbét,
+
+kobszam n = [i ^ 3 | i <- [1 .. n]]
 -- - az első n olyan természetes számot, amelyben nem szerepelnek a négyzetszámok,
+
+nemNegyzet n = [i | i <- [ 1..n], i /= (sqrt i^2)]
 -- - x hatványait adott n-ig,
+
+hatvany x n = [x ^ i | i <- [1..n]]
 -- - egy szám páros osztóinak listáját,
+
+parosOsztok x = [i | i <- [1..x], mod x i == 0, mod i 2 == 0]
+
+parosOsztok2 x = [i | i <- [1 .. x],mod x i == 0, even i]
 -- - n-ig a prímszámok listáját,
+
+osztok x = [i | i <-[1..x],mod x i == 0 ]
+
+primszam x = osztok x == [1,x]
+
+primszamN n = [i | i<- [ 1..n],primszam i]
+
+primszanN2 n = [i | i <- [ 1.. n], primszamL i]
+    where
+        primszamL sn = [1,sn] == osztokL sn
+        osztokL sn2 = [j | j <- [1..sn2], mod sn2 j ==0]
 -- - n-ig az összetett számok listáját,
+
+oszetett n = [i | i <- [1..n],primszam i == False]
+
+oszetett2 n = [i | i <- [1..n], not (primszam i)]
 -- - n-ig a páratlan összetett számok listáját,
+
+paratlanOszetett n = [i | i <- [ 1.. n], not (primszam i), mod i 2 /= 0]
+
+paratlanOszetett2 n = [i | i <- [ 1.. n], not (primszam i), not (even i)]
+
+paratlanOszetett3 n = [i | i <- [ 1.. n], not (primszam i), odd i]
+
+
 -- - az n-nél kisebb Pitágorászi számhármasokat,
+
+pitagorasz n = [(a,b,c) | c <- [1 .. n], b<- [ 1..c],a <-[1..b]]
 -- - a következő listát: $$[(\texttt{a},0), (\texttt{b},1),\ldots, (\texttt{z}, 25)]$$,
+
+betuSzam = zip ['a' .. 'z'][0..25]
+
+betuSzam2 = zip ['a' .. 'z'][25,24 ..]
 -- - a következő listát: $$[(0, 5), (1, 4), (2, 3), (3, 2), (4, 1), (5, 0)]$$, majd általánosítsuk a feladatot.
+
+szamok = zip [ 0..5][5,4..0]
+
+szamok2 n = zip [0 ..n][n,n -1 .. 0]
+
+szamok3 n = [(i, n-1)| i<-[0..n]]
 -- - azt a listát, ami felváltva tartalmaz True és False értékeket.
 
+
+main = do
+    putStr "elempar (6,7)(7,6): "
+    print (elempar(6,7)(7,6))
+    print (elempar2 (6,7)(4,7))
+    let ep1 = (6,7)
+        ep2 = (4,7)
+    putStrLn ("elempar (6,7)(4,7) " ++ show (elempar2 (6,7)(4,7)))
---
+++ 02.labor/lab2_KBT.hs
@@ -0,0 +1,15 @@
+import Text.Parsec.Token (GenLanguageDef(reservedNames))
+
+
+szjSzorzat 0 = 1
+szjSzorzat n = mod n 10 * szjSzorzat (div n 10)
+
+szjSzorzat2 n
+    | n < 0 = error "neg. szam"
+    | div n 10 == 0 = n
+    | otherwise = mod n 10 * szjSzorzat2 (div n 10)
+
+szjSzorzat3 n res
+    | n < 0 = error "neg. szam"
+    | div n 10 == 0 = res * n
+    | otherwise = szjSzorzat3 (div n 10) (res * (mod n 10))
